f2498ea7d9062ba1377615dc0ea9f32b36390ff4
James Blacklock <jamesblacklock@protonmail.com>
2022-02-23 12:51:12 -0500

    committing to "lecture"

--- car/car.test.js
+++ car/car.test.js
@@ -9,4 +9,10 @@ describe('Car class test', () => {
         let car = new Car('toyota', 'prius');
         expect(car).toBeInstanceOf(Car);
     });
+
+    test('Car make & model', () => {
+        let car = new Car('toyota', 'prius');
+        expect(car.make).toBe('toyota');
+        expect(car.model).toBe('prius');
+    });
 });
